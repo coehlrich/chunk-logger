aab9e155debfd1a0f68796dc404c54602737812f
coehlrich <charlieloehlrich@gmail.com>
2017-06-17 13:21:59 +1200

    change to version 1.1.0

--- src/main/java/com/coehlrich/chunklogger/ChunkLogger.java
+++ src/main/java/com/coehlrich/chunklogger/ChunkLogger.java
@@ -18,7 +18,7 @@ public class ChunkLogger {
 	
 	public static final String MODID = "chunklogger";
 	public static final String MODNAME = "Chunk Logger";
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "1.1.0";
 	
 	@Mod.Instance(MODID)
 	public static ChunkLogger instance;
