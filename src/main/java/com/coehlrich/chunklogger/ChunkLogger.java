a5aeba1f900d3e4b0c072d8fa75289445a90716e
coehlrich <charlieloehlrich@gmail.com>
2017-10-14 22:01:10 +1300

    update to 1.3

--- src/main/java/com/coehlrich/chunklogger/ChunkLogger.java
+++ src/main/java/com/coehlrich/chunklogger/ChunkLogger.java
@@ -21,7 +21,7 @@ public class ChunkLogger {
 	
 	public static final String MODID = "chunklogger";
 	public static final String MODNAME = "Chunk Logger";
-	public static final String VERSION = "1.2.0";
+	public static final String VERSION = "1.3";
 	
 	public static Logger logger;
 	
